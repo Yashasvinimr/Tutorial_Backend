e5558fc5310dacb42b60663740600992f20ea354
Yashasvinimr <yashasvinimr.cs21@bmsce.ac.in>
2025-03-20 09:56:11 +0530

    first commit for front end

--- tutorial-frontend/src/app/app.routes.ts
+++ tutorial-frontend/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { TutorialsListComponent } from './tutorials-list/tutorials-list.component';
+import { TutorialsDetailsComponent } from './tutorials-details/tutorials-details.component';
 
 export const routes: Routes = [
     { path:'', redirectTo: 'tutprials', pathMatch:'full'},
-    { path: 'tutorials', component: TutorialsListComponent}
-    
+    { path: 'tutorials', component: TutorialsListComponent},
+    {path: 'tutorials/:id', component: TutorialsDetailsComponent}
 ];
